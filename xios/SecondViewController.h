15d3fc984c1a5152c907fc60ee030e3ec3e18735
rory.zhang <coolrl@126.com>
2017-02-26 06:04:31 -0800

    增加UISlider&UIProgressView

--- xios/SecondViewController.h
+++ xios/SecondViewController.h
@@ -10,12 +10,23 @@
 
 @interface SecondViewController : UIViewController {
     
+    //选择器
     UISwitch *_switcher;
+    //进度条
+    UIProgressView *_progresser;
+    //滑动条
+    UISlider *_slider;
+    //步进器
+    UIStepper *_stepper;
+    //分栏控制器
+    UISegmentedControl *_segmenter;
     
 }
 
 @property (retain, nonatomic) UISwitch *switcher;
-
-
+@property (retain, nonatomic) UIProgressView *progresser;
+@property (retain, nonatomic) UISlider *slider;
+@property (retain, nonatomic) UIStepper *stepper;
+@property (retain, nonatomic) UISegmentedControl *segmenter;
 
 @end
--- xios/SecondViewController.m
+++ xios/SecondViewController.m
@@ -15,6 +15,10 @@
 @implementation SecondViewController
 
 @synthesize switcher = _switcher;
+@synthesize progresser = _progresser;
+@synthesize slider = _slider;
+@synthesize stepper = _stepper;
+@synthesize segmenter = _segmenter;
 
 // 屏幕被点击时调用
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
@@ -41,6 +45,41 @@
     [_switcher setTintColor: [UIColor purpleColor]];
     [_switcher addTarget:self action:@selector(changeSwitcher:) forControlEvents:UIControlEventValueChanged];
     [self.view addSubview:_switcher];
+    
+    _progresser = [[UIProgressView alloc] init];
+    _progresser.frame = CGRectMake(10, 100, 200, 40);
+    // 进度颜色
+    _progresser.progressTintColor = [UIColor redColor];
+    // 默认颜色
+    _progresser.trackTintColor = [UIColor greenColor];
+    _progresser.progress = 0.7;
+    _progresser.progressViewStyle = UIProgressViewStyleDefault;
+    [self.view addSubview:_progresser];
+    
+    _slider = [[UISlider alloc] init];
+    _slider.frame = CGRectMake(10, 160, 200, 40);
+    _slider.minimumValue = 0;
+    _slider.maximumValue = 100;
+    _slider.value = 30;
+    //滑块颜色
+    _slider.thumbTintColor = [UIColor blueColor];
+    //滑块左侧颜色
+    _slider.minimumTrackTintColor = [UIColor redColor];
+    //滑块右侧颜色
+    _slider.maximumTrackTintColor = [UIColor greenColor];
+    [_slider addTarget:self action:@selector(changeSlider) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:_slider];
+    
+    _stepper = [[UIStepper alloc] init];
+    _stepper.frame = CGRectMake(10, 220, 200, 40);
+    _stepper.minimumValue = 0;
+    _stepper.maximumValue = 100;
+    _stepper.value = 10;
+    _stepper.stepValue = 1;
+    _stepper.autorepeat = YES;
+    _stepper.continuous = YES;
+    [_stepper addTarget:self action:@selector(changeStepper) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:_stepper];
 }
 
 // 内存不足时被调用
@@ -56,6 +95,15 @@
     } else {
         NSLog(@"switcher is closed.");
     }
+}
+
+- (void) changeSlider {
+    NSLog(@"slider value=%f", _slider.value);
+    _progresser.progress = (_slider.value - _slider.minimumValue) / (_slider.maximumValue - _slider.minimumValue);
+}
+
+- (void) changeStepper {
+    NSLog(@"change stepper. value=%f.", _stepper.value);
 }
 
 /*
